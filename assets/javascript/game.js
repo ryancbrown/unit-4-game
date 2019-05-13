f49ff52693bea73d64bcf72bc562bab6dea75446
rcbnyc <16548379+ryancbrown@users.noreply.github.com>
2019-05-12 23:28:20 -0400

    add instructions modal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,87 +1,86 @@
 
 var w = 0; 
 var l = 0;
-var values = [];
-var game = setupGame();
+var values = []; // Gem values
+var game = setupGame(); // Jump to L8
 
-// start game
 function setupGame() { 
     var game = { 
         wins: w,
         losses: l,
-        round: randomNumber()
+        round: randomNumber() // Jump to L32
     };
 
-    // generate random game values
+    // Gem values
     for (i = 0; i < 4; i++) { 
-        if (values.length < 4) {
-            values.push(Math.floor(Math.random() * 7) + 1) // Random number btwn 1 and 7
-        };
+        values.push(Math.floor(Math.random() * 7) + 1) // Random: 1-7
     };
     
-    // print gem images
+    // Print gem + value on page
     for (var i = 0; i < 4; i++) {
         $('#gems').prepend('<div class="c"><img src="assets/images/gem' + i + '.png" /><p>' + game.round.gem[i] + '</p></div>')
     }; 
 
+    // Show game value on page
     $('#goal').text(game.round.number)
 
     return game
 };
 
-// create a random number generator 
+// Generate a random number to act as game value
 function randomNumber() {
     var round = { 
-        number: Math.floor(Math.random() * 40) + 10, // random number btwn 10 and 50
+        number: Math.floor(Math.random() * 40) + 10, // Random: 10-50
         gem: values, 
         score: 0
     }; 
     return round
 };
 
+// Check if the player has won
 function hasWon(score) { 
     if(score === game.round.number) { 
-        // increase win count
-        w++ 
-        // alert if won
-        alert('Winner')
-        // reset game
-        resetGame()
+        w++ // Increase win count
+        alert('Nice job! You found the correct gem combination for ' + game.round.number + '.' )
+        resetGame() // Jump to L68
     } else {
-        // if score is not equal to the number chosen, then run hasLost
-        hasLost(score)
+        hasLost(score) // Jump to L56
     };  
 };
 
 function hasLost(score) {
     if(score > game.round.number) { 
-        // increase loss count
-        l++
-        // alert if lost
-        alert('Loser')
-        // reset game
-        resetGame()
+        l++ // increase loss count
+        alert(score + ' is greater than ' + game.round.number + '. Let\'s try this again.')
+        resetGame() // Jump to L68
     };
 };
 
+// Reset the game once won or lost
 function resetGame() {
-    values = [];
-    game.round.score = 0;
-    $('.c').detach();
-    game = setupGame();
+    values = []; // Clear old gem values
+    game.round.score = 0; // Set player score to 0
+    $('.c').detach(); // Clear old gems on page
+    game = setupGame(); // Create new round (Jump to L8)
 
+    // Update page
     $('#score').empty().text(game.round.score)
     $('#goal').empty().text(game.round.number)  
     $('#wins').empty().text('Wins: ' + w) 
     $('#losses').empty().text('Losses: ' + l) 
 };
 
+// Click listener
 $('#gems').on('click', '.c', function() { 
-    console.log($('.c'))
-    // On click update score
+    // on click update player score
     game.round.score = parseInt(game.round.score) + parseInt($(this).text())
-    // Print score on page
+    // print score on page
     $('#score').html(game.round.score)
-    // Check if the user has won with their last guess
+    // check if the player has won with their last guess
     hasWon(game.round.score)
 });
+
+// Hide the instructions when start-game button is clicked
+$('#start-game').on('click', function(){
+    $('#dim').css("display", "none")
+})
